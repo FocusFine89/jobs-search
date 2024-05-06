b84da0b0637693f9a0f96559a786fb99b3a8afe0
FocusFine89 <niivanov02@gmail.com>
2024-05-06 17:04:13 +0200

    tutto

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,33 @@
+const initialState = {
+  preferiti: {
+    content: [],
+  },
+};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_PREFERITI":
+      return {
+        ...state,
+        preferiti: {
+          ...state.preferiti,
+          content: [...state.preferiti.content, action.payload],
+        },
+      };
+
+    case "REMOVE_PREFERITI":
+      return {
+        ...state,
+        preferiti: {
+          ...state.preferiti,
+          content: state.preferiti.content.filter(
+            (_, i) => i !== action.payload
+          ),
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
